74368ca9933cd6c74327ce2664073feeb0771e7f
Shinya Tahara <tahshiny@amazon.co.jp>
2021-01-09 11:50:50 +0900

    implement reader functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -19,10 +21,22 @@ struct Opts {
 
 fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        // ファイルを指定しなかった場合
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
     }
-    
-    println!("Is verbosity specified?: {}", opts.verbose);
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
